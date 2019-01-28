a300fc358347ec9209b23b255ca9f1c9ca39e0d0
Emily Schaumann <42627450+eschaumann@users.noreply.github.com>
2019-01-28 15:51:00 -0700

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -1,9 +1,4 @@
-declare -a sentences=("عائلة"
-			"أنا"
-			"نحن")
-
-
-for sent in "${sentences[@]}"
+cat test_sents.txt | while read sent
 do
 	echo -e ${sent}$"\t\t"$(echo ${sent} | lt-proc msa-lev.automorf.bin |   gawk 'BEGIN{RS="$"; FS="/";}{nf=split($1,COMPONENTS,"^"); for(i = 1; i<nf; i++) printf COMPONENTS[i]; if($2 != "") printf("^%s$",$2);}' |   apertium-transfer apertium-msa-lev.msa-lev.t1x msa-lev.t1x.bin msa-lev.autobil.bin  |   lt-proc -g msa-lev.autogen.bin)
 done
